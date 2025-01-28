6f3760610eca27f4ef788e16e0603aaccd5a347b
Egawa Takashi <egawa.takashi@gmail.com>
2025-01-28 23:25:03 +0900

    add type app

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"syscall/js"
 )
 
+type dom struct {
+	btnGenerate js.Value
+}
+
+type app struct {
+	el dom
+}
+
+func (a *app) Start() {
+	a.el.btnGenerate = js.Global().Get("document").Call("getElementById", "generate")
+	a.el.btnGenerate.Call("addEventListener", "click", js.FuncOf(a.generate))
+}
+
+func (a *app) generate(this js.Value, p []js.Value) interface{} {
+	fmt.Println("Generate Button clicked")
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	a := app{}
+	a.Start()
+
+	<-make(chan struct{})
 }
