a1587c021385dd325787050418b25e267570a63f
Egawa Takashi <egawa.takashi@gmail.com>
2025-02-24 01:10:21 +0900

    fix typo

--- localserver/run_server.go
+++ localserver/run_server.go
@@ -15,7 +15,7 @@ func main() {
 	fs := http.FileServer(http.Dir(publishDir))
 	http.Handle("/", fs)
 
-	log.Printf("Running HTTP Server on http://localhost:%s/colortrace.html", port)
+	log.Printf("Running HTTP Server on http://localhost:%s/colortress.html", port)
 	if err := http.ListenAndServe(hostport, nil); err != nil {
 		log.Fatal(err)
 	}
