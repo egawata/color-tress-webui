af8dad281e769cefa2bb66a6d45643746228113b
Egawa Takashi <egawa.takashi@gmail.com>
2025-02-27 02:22:18 +0900

    change build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,19 +6,21 @@
 set -e
 
 files=(
-    main.wasm
-    wasm_exec.js
     colortress.html
     generating.gif
     noimage.png
 )
 dest="./build"
 
-cp "$(tinygo env TINYGOROOT)/targets/wasm_exec.js" ./wasm_exec.js
-tinygo build -o main.wasm -target wasm ./main.go
-
 mkdir -p $dest
-rm -f $dest/*
+rm -f ${dest}/*
+
+cp "$(go env GOROOT)/lib/wasm/wasm_exec.js" ${dest}/wasm_exec.js
+GOOS=js GOARCH=wasm go build -o ${dest}/main.wasm main.go
+
+#cp "$(tinygo env TINYGOROOT)/targets/wasm_exec.js" ${dest}/wasm_exec.js
+#cp ./my_wasm_exec.js ${dest}/wasm_exec.js
+#tinygo build -o ${dest}/main.wasm -target wasm ./main.go
 
 for file in "${files[@]}"; do
     cp $file $dest
