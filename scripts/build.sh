7ff2715441035ef7206e98341a91da26003f317c
Egawa Takashi <egawa.takashi@gmail.com>
2025-02-27 20:38:19 +0900

    build with tinygo

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,12 +15,11 @@ dest="./build"
 mkdir -p $dest
 rm -f ${dest}/*
 
-cp "$(go env GOROOT)/lib/wasm/wasm_exec.js" ${dest}/wasm_exec.js
-GOOS=js GOARCH=wasm go build -o ${dest}/main.wasm main.go
+#cp "$(go env GOROOT)/lib/wasm/wasm_exec.js" ${dest}/wasm_exec.js
+#GOOS=js GOARCH=wasm go build -o ${dest}/main.wasm main.go
 
-#cp "$(tinygo env TINYGOROOT)/targets/wasm_exec.js" ${dest}/wasm_exec.js
-#cp ./my_wasm_exec.js ${dest}/wasm_exec.js
-#tinygo build -o ${dest}/main.wasm -target wasm ./main.go
+cp "$(tinygo env TINYGOROOT)/targets/wasm_exec.js" ${dest}/wasm_exec.js
+tinygo build -o ${dest}/main.wasm -target wasm ./main.go
 
 for file in "${files[@]}"; do
     cp $file $dest
